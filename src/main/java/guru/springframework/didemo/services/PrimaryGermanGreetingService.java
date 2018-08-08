aae491ccc532625b8f6c67e2552e305078c64109
Costel.Istrate <Costel.Istrate@orange.com>
2018-08-08 11:17:12 +0300

    moved all the services configuration to Config class and instantiate the services for the factory class

--- src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -1,13 +1,11 @@
 package guru.springframework.didemo.services;
 
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
+/*
 @Service
 @Primary
 @Profile("de")
+*/
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
